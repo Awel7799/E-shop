174f3b01478112c44a8eefdf8ae6945fb24d27e6
Awel7799 <awelkassim45@gmail.com>
2025-04-21 04:54:25 -0700

    product page completed

--- public/product.php
+++ public/product.php
@@ -3,134 +3,335 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product-details</title>
-    <link rel="stylesheet" href="home.css">
+    <title>Products</title>
     <link rel="stylesheet" href="product.css">
-    <link rel="stylesheet" href="products-detail.css">
+    <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="query.css">
 </head>
 <body>
-    <header class="product-header">
-        <div class="logo-text">E-SHOP</div>
     
-        <form action="">
-            <button><img src="icons/search.png" alt=""></button>
-            <input type="search" placeholder="Search Products">
-        </form>
-        <div class="links">
-                <a class="hidden-tab"  href="index.php">HOME</a>
-                <a class="active hidden-tab" href="products-detail.php">PRODUCT</a>
-                <a class="hidden-tab" href="#">ABOUT</a>
-                <a class="hidden-tab" href="#">CONTACT</a>
-                <a class="hidden-tab3" href="#"><img src="./assets/icons/shop.png" alt=""></a>
-                <a class="hidden-tab3" href="#"><img src="./assets/icons/person.png" alt=""></a>
-        </div>   
-    </header>
-     <div class="bag_info">
+<header class="product-header">
+    <div class="logo-text">E-SHOP</div>
 
-        <img src="./assets/images/bag.png" alt="" class="big_bag">
-        <p class="HB dd">Home/Bags</p>
-        <h1 class="bag_title dd">CHANEL Mini Flap Bag</h1>
-        <p class="bag_price  dd">9900000 MMK</p>
-        <select class="select  dd" ><option>select size</option></select>
-        <H3 class="product_details dd">PRODUCT DETAILS</H3>
-        <p class="details dd">
-            Lorem ipsum dolor sit amet consectetur, 
-            adipisicing elit. Iusto perferendis 
-            cum omnis provident possimus praesentium unde, 
-            adipisci consequuntur eaque doloribus!
-        </p>
-        <p class="counter dd">- 1  +</p>
-        <input type="button dd" value="Add To Cart" class="red_button">
+    <form action="">
+        <button><img src="icons/search.png" alt=""></button>
+        <input type="search" placeholder="Search Products">
+    </form>
+    <div class="links">
+            <a class="hidden-tab"  href="index.html">HOME</a>
+            <a class="active hidden-tab" href="products.html">PRODUCT</a>
+            <a class="hidden-tab" href="#">ABOUT</a>
+            <a class="hidden-tab" href="#">CONTACT</a>
+            <a class="hidden-tab3" href="#"><img src="./assets/icons/shop.png" alt=""></a>
+            <a class="hidden-tab3" href="#"><img src="./assets/icons/person.png" alt=""></a>
+    </div>   
+</header>
+
+<section class="body">
+    <div class="left hidden-tab">
+
+        <section class="categories">
+            <h2 class="line">Categories</h2>
+            <ul>
+                <li>Clothes</li>
+                <li>Watches</li>
+                <li>Bags</li>
+                <li>Glasses</li>
+                <li>Cosmetics</li>
+                <li>Perfumes</li>
+                <li>Footwear</li>
+            </ul>
+        </section>
+
     </div>
-    <div class="bag_images">
-        <img src="./assets/images/bag 4.png" alt="">
-        <img src="./assets/images/bag 3.png" alt="">
-        <img src="./assets/images/bag 2.png" alt="">
-        <img src="./assets/images/bag 1.png" alt="">
-    </div>
-    <div class="rproducts">
-        <h1 ><span class="line">Relative</span> products</h1>
-    </div>
-    <div class="rproducts">
-        <div class="picbg ">
-            <img src="./assets/images/hat.png" alt="">
-            <h2 class="akub">Akubara Snowy River Hat</h2>
-            <p class="price">520000</p>
-            <p class="curr"><b>MMK</b></p>
-            <p class="plusbtn">+</p>
+
+
+
+    <div class="right">
+
+        <h1><span class="line">All Products</span></h1>
+
+        <div class="sort">
+
+            <form action="">
+                <select name="" id="">
+                    <option>Sort by Price</option>
+                </select>
+            </form>
+
         </div>
-        <div class="picbg ">
-            <img src="./assets/images/watch.png" alt="">
-            <h2 class="akub">G-Shock GA-100GB-1A</h2>
-            <p class="price">640000</p>
-            <p class="curr"><b>MMK</b></p>
-            <p class="plusbtn">+</p>
-        </div>
-        <div class="picbg ">
-            <img src="./assets/images/bag.png" alt="">
-            <h2 class="akub">CHANEL Mini flap bag</h2>
-            <p class="price">990000</p>
-            <p class="curr"><b>MMK</b></p>
-            <p class="plusbtn">+</p>
-        </div>
-        <div class="picbg ">
-            <img src="./assets/images/allstr.png" alt="">
-            <h2 class="akub">Converse Brand Allstar Shoes</h2>
-            <p class="price">540000</p>
-            <p class="curr"><b>MMK</b></p>
-            <p class="plusbtn">+</p>
-        </div>
-    </div>
-    <footer>
-        <div class="top wrap2">
-    
-            <div class="f-logo hidden-tab">
-                <span>E-shop</span>
-                Specializes in providing high-quality,stylish
-                product for your wardrobe 
+
+        <section>
+
+            <div class="item-cards">
+
+                <a href="product-detail.html"><img src="./assets/images/bag.png" alt=""></a>
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
             </div>
-    
-            <div class="contact ">
-                <div class="title line">CONTACT</div>
-    
-                <div class="co-info">
-                    <img src="./assets/icons/location.png" alt="kiya">
-                    <section><p>No. 164, Changyisu Str, Paung Township,</p><p>Mon State, Myanmar</p></section>
-                </div>
-                <div class="co-info">
-                    <img src="./assets/icons/phone.png" alt="">
-                    <p>09-97387167, </p><p>09-91801383</p>
-                </div>
-                <div class="co-info">
-                    <img src="./assets/icons/email.png" alt="">
-                    <p>hakunlatena@gmail.com</p>
-                </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/allstr.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
             </div>
-            <div class="quick-link">
-                <div class="title line">QUICK LINK</div>
-                <ul>
-                    <li>Home</li>
-                    <li>Products</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Add To Cart</li>
-                </ul>
-                </div>
-            <div class="follow-us">
-                <div class="title line">Follow Us</div>
-                <img src="./assets/icons/facebook.png" alt="">
-                <img src="./assets/icons/twitter.png" alt="">
-                <img src="./assets/icons/instagram.png" alt="">
-                <img src="./assets/icons/youtube.png" alt="">
+
+            <div class="item-cards">
+
+                <img src="./assets/images/hoodie.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/perfume.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+
+        <!-- </section> -->
+
+
+        <!-- <section> -->
+
+            <div class="item-cards">
+
+                <img src="./assets/images/hat.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/watch.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/bag.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/allstr.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+
+        <!-- </section> -->
+
+
+        <!-- <section> -->
+
+            <div class="item-cards">
+
+                <img src="./assets/images/bag.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/allstr.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/hoodie.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/perfume.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+
+        <!-- </section> -->
+
+
+        <!-- <section> -->
+
+            <div class="item-cards">
+
+                <img src="./assets/images/hat.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/watch.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/bag.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/allstr.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+
+        <!-- </section> -->
+
+
+        <!-- <section> -->
+
+            <div class="item-cards">
+
+                <img src="./assets/images/bag.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/allstr.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/hoodie.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+            <div class="item-cards">
+
+                <img src="./assets/images/perfume.png" alt="">
+                <span class="item-name">CHANEL Mini flap bag</span>
+                
+                <div class="price"><p><span>9900000</span> MMk</p><button class="add-to-cart">+</button> </div>
+
+            </div>
+
+
+        </section>
+
+        <button class="b1"><</button>
+        <button class="bn">1</button>
+        <button class="bn red">2</button>
+        <button class="bn">3</button>
+        <button class="bn">4</button>
+        <button class="bn">5</button>
+        <button class="b1">></button>
+
+    </div>
+
+</section>
+
+<footer>
+    <div class="top wrap2">
+
+        <div class="f-logo hidden-tab">
+            <span>E-shop</span>
+            Specializes in providing high-quality,stylish
+            product for your wardrobe 
+        </div>
+
+        <div class="contact ">
+            <div class="title line">CONTACT</div>
+
+            <div class="co-info">
+                <img src="./assets/icons/location.png" alt="kiya">
+                <section><p>No. 164, Changyisu Str, Paung Township,</p><p>Mon State, Myanmar</p></section>
+            </div>
+            <div class="co-info">
+                <img src="./assets/icons/phone.png" alt="">
+                <p>09-97387167, </p><p>09-91801383</p>
+            </div>
+            <div class="co-info">
+                <img src="./assets/icons/email.png" alt="">
+                <p>hakunlatena@gmail.com</p>
             </div>
         </div>
-        <hr>
-        <div class="bottom">
-            Copyright ©2023 <span>E-SHOP</span>. All right reserved
+        <div class="quick-link">
+            <div class="title line">QUICK LINK</div>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Add To Cart</li>
+            </ul>
         </div>
-        <hr>
-    </footer>
+        <div class="follow-us">
+            <div class="title line">Follow Us</div>
+            <img src="./assets/icons/facebook.png" alt="">
+            <img src="./assets/icons/twitter.png" alt="">
+            <img src="./assets/icons/instagram.png" alt="">
+            <img src="./assets/icons/youtube.png" alt="">
+        </div>
+    </div>
+    <hr>
+    <div class="bottom">
+        Copyright ©2023 <span>E-SHOP</span>. All right reserved
+    </div>
+    <hr>
+</footer>
+
 
 </body>
 </html>
